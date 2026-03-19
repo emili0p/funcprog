3ac0ce46e0c0989e76ece004b3d2e4cb880fe1ea
emili0p <emiliioizq23@gmail.com>
2026-03-18 21:31:01 -0600

    Ejercicos de hoy

---
+++ ejemplosclase/ejer1.scala
@@ -0,0 +1,18 @@
+:load lazy.scala
+a
+a
+a
+a
+:load lazy.scala
+:load lazy.scala
+val nombre = "Emilio"
+val apellido = "Izquierdo"
+val nombrecompleto = nombre + apellido
+val apellido = "Rodriguez"
+val nombrecompleto = nombre + apellido
+var edad = 20
+val edad = 20
+val javaVersion = util.Properties.javaVersion
+util.Properties.javaVendor
+util.Properties.javaVmName
+lazy val resultado = 10/0
---
+++ ejemplosclase/lazy.scala
@@ -0,0 +1,10 @@
+lazy val x = {
+  println("ejecutando proceso costoso")
+  100
+}
+
+println("Inicio")
+println(x)
+println("el valor de x  es " ,  x)
+
+
