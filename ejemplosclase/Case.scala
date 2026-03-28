a95545b75786bcef3d72718795aeeb8ab79bd33b
emili0p <emiliioizq23@gmail.com>
2026-03-27 19:54:50 -0600

    Ejemplo case

---
+++ ejemplosclase/Case.scala
@@ -0,0 +1,8 @@
+val nombre = "Pedro"
+println(s"Hola $nombre")
+val precio = 100392.03490394
+println(f"el precio del barril de cerveza es $precio%.2f")
+
+case class Alumno(nombre: String, edad: Int, tieneCredencial: Boolean)
+
+val Alumnos = Array(Alumno("juan", 19, false), Alumno("diego", 19, false))
